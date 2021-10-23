f9667f199a30839c7d089fbd128f293159c6f9f5
chuangyu-hscy <rin@chuangyu-hscy.com>
2021-10-23 23:04:09 +1100

    Updated page title & favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // import required libraries
+import React, { useEffect } from "react";
 import "./App.css";
 
 import Profile from "./profile/profile.jsx";
 
 function App() {
+  useEffect(() => {
+    document.title = "Rin";
+  }, []);
   return (
     <div className="App">
       <Profile />
