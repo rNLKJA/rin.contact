0cd36969e5a483361ad8355a3407780b7e751654
chuangyu-hscy <rin@chuangyu-hscy.com>
2021-10-23 19:05:07 +1100

    Initialise my personal react website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import required libraries
+import "./App.css";
+import profileImage from "./img/profileImage.jpg";
+import twitterImage from "./img/twitter.png";
+import githubImage from "./img/github.png";
+import linkedInImage from "./img/linkedIn.png";
+import instagram from "./img/instagram.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="personal-profile">
+        <div className="profileImage-container">
+          <img
+            className="profileImage"
+            src={profileImage}
+            alt="rin-huang profile"
+          />
+        </div>
+        <h1>Sunchuangyu Huang</h1>
+        <LinkContainer />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+export const LinkContainer = () => {
+  return (
+    <div className="link-container">
+      <div className="link-item">
+        <a href="https://github.com/chuangyu-hscy" alt="github">
+          <img src={githubImage} alt="github" className="github" />
+        </a>
+      </div>
+      <div className="link-item">
+        <a href="https://www.linkedin.com/in/chuangyu-hscy" alt="linkedIn">
+          <img src={linkedInImage} alt="linkedIn" />
+        </a>
+      </div>
+      {/* <div className="link-item"><img src={discordImage} alt="discord" /></div> */}
+      <div className="link-item">
+        <a href="https://www.instagram.com/chuangyu_hscy/" alt="instagram">
+          <img src={instagram} alt="instagram" />
+        </a>
+      </div>
+      <div className="link-item">
+        <a href="https://twitter.com/chuangyu_hscy" alt="instagram">
+          <img src={twitterImage} alt="instagram" />
+        </a>
+      </div>
+    </div>
+  );
+};
