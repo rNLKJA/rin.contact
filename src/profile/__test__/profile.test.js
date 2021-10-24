ab1395bdca0f01f38dd3cd5b1b07a7aedd02667b
chuangyu-hscy <rin@chuangyu-hscy.com>
2021-10-24 14:34:30 +1100

    removed unused console.log

--- src/profile/__test__/profile.test.js
+++ src/profile/__test__/profile.test.js
@@ -6,6 +6,6 @@ import "@testing-library/jest-dom/extend-expect";
 test("Profile Card renders with correct text", () => {
   const component = render(<Profile />);
   const author = component.getByTestId("author");
-  // console.log(author);
+
   expect(author.textContent).toBe("Sunchuangyu Huang");
 });
