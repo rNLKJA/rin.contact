3cbe41fe751a02e0adcabb54d190744bcba71abb
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2022-03-05 20:53:57 +1100

    Updated app.tsx ts comments

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,8 +2,12 @@
 import { useEffect, FC } from "react";
 import "./App.css";
 
+// config dotenv which enable the react program read parameters from .env file
+require("dotenv").config();
+
 // import ESSENTIAL Profile component
 import Profile from "../profile/Profile";
+import { stringify } from "querystring";
 
 /**
  * This is the Main function which contains the Profile component and
@@ -11,8 +15,10 @@ import Profile from "../profile/Profile";
  * @returns App
  */
 const App: FC = () => {
+  const profileTitle: string = "Rin Huang"; // change your page title at here!
+
   useEffect(() => {
-    document.title = "Rin";
+    document.title = profileTitle;
   }, []);
 
   return <Profile />;
