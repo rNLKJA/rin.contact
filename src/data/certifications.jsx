0c5a0b9217b9323a384a5810c1094c52e418e767
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2022-12-28 02:44:17 +1100

    Updates

--- src/data/certifications.jsx
+++
@@ -1,8 +0,0 @@
-const certification_data = [
-  "Google Data Analytic Certificate",
-  "Google IT Automation with Python",
-  "Agile with Atlassian Jira",
-  "Victoria Driver License",
-];
-
-export default certification_data;
