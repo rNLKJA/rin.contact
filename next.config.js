296410ddcca8d749d1768b9988e80de2619d8ebe
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-12-29 03:54:58 +1100

    updated config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const nextConfig = {
   trailingSlash: true,
-  output: "standalone",
-  basePath: "/rin.contact", // Replace 'project-name' with your GitHub repository name
-  assetPrefix: "/rin.contact/", // Same as basePath but with a trailing slash
+  output: "export",
+  images: { unoptimized: true },
 };
 module.exports = nextConfig;
