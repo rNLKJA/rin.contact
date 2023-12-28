1a78b597754751c452b937461c1682c01135fcd2
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-12-29 02:46:33 +1100

    updated config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const isProd = process.env.NODE_ENV === "production";
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Only add the basePath and assetPrefix if it's in production
-  basePath: isProd ? "/rin.contact" : "",
-  assetPrefix: isProd ? "/rin.contact/" : "",
+  trailingSlash: true,
   output: "export",
-  // Additional configurations can go here
-  reactStrictMode: true,
 };
-
 module.exports = nextConfig;
