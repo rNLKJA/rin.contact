c7029dbaa5332da343598641c4451a8bde7be6ec
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2023-12-30 16:25:07 +1100

    Create template.jsx

---
+++ components/layout/blog/template.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, use } from "react";
+import { Fade } from "react-awesome-reveal";
+
+export default function BlogContent() {
+  const [blogContent, setBlogContent] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/blogs/new-year-milestone.json")
+      .then((response) => response.json()) // directly parsing the JSON here
+      .then((jsonData) => setBlogContent(jsonData))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <Fade triggerOnce duration={1500} direction="left">
+      <div>
+        <h1>Blog Content</h1>
+        <p></p>
+      </div>
+    </Fade>
+  );
+}
