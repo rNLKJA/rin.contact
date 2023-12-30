a9648296946b14b824c6ab03b83b15164b04f61d
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2023-12-30 16:25:03 +1100

    Create blogList.jsx

---
+++ components/layout/blog/blogList.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { Fade } from "react-awesome-reveal";
+
+export default function BlogList() {
+  const [blogList, setBlogList] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/blog_list.json")
+      .then((response) => response.json())
+      .then((data) => setBlogList(data.reverse()))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Blog List</h2>
+      <br />
+      <div className="flex flex-col gap-8">
+        {blogList &&
+          blogList.map((blog) => (
+            <Fade triggerOnce duration={1500} direction="up">
+              <Link href={blog.link}>
+                <div key={blog.title}>
+                  <h3 className="link-hover">{blog.title}</h3>
+                  <div className="flex md:flex-row gap-4">
+                    <p>{blog.author}</p>
+                    <p className="italic">{blog.date}</p>
+                  </div>
+                  <p className="leading-5 text-hover">{blog.brief}</p>
+                </div>
+              </Link>
+            </Fade>
+          ))}
+      </div>
+    </div>
+  );
+}
