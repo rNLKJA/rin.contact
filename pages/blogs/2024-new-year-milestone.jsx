c8a70dfabbd05c99b6c6a6b91082cd15354d7810
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2023-12-30 16:25:31 +1100

    Create 2024-new-year-milestone.jsx

---
+++ pages/blogs/2024-new-year-milestone.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { Fade } from "react-awesome-reveal";
+
+export default function Home() {
+  const [blogContent, setBlogContent] = useState(null);
+
+  useEffect(() => {
+    fetch("/data/blogs/new-year-milestone.json")
+      .then((response) => response.json()) // directly parsing the JSON here
+      .then((jsonData) => setBlogContent(jsonData))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      {blogContent && (
+        <Fade triggerOnce duration={1500} direction="up">
+          <div className="flex flex-col p-5">
+            <h1 className="text-3xl font-bold">{blogContent.title}</h1>
+            <br />
+            <div className="flex flex-row gap-5">
+              <p>{blogContent.author}</p>
+              <p>{blogContent.date}</p>
+            </div>
+            <div className="mt-4 flex flex-col gap-2">
+              {blogContent.content.map((paragraph, index) => (
+                <p key={index} className="my-2 text-justify leading-6">
+                  {paragraph}
+                </p>
+              ))}
+            </div>
+          </div>
+        </Fade>
+      )}
+    </div>
+  );
+}
