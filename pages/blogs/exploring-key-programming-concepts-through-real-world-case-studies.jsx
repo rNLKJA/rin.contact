3f9427157b803a88e855ebf243c63f192ae24e42
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-01 21:20:49 +1100

    Created new blog content

---
+++ pages/blogs/exploring-key-programming-concepts-through-real-world-case-studies.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, use } from "react";
+import { Fade } from "react-awesome-reveal";
+import Link from "next/link";
+import Typography from "@mui/material/Typography";
+
+export default function BlogContent() {
+  const [blogContent, setBlogContent] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "/data/blogs/exploring-key-programming-concepts-through-real-world-case-study.json",
+    )
+      .then((response) => response.json()) // directly parsing the JSON here
+      .then((jsonData) => setBlogContent(jsonData))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <Fade triggerOnce duration={1500} direction="left">
+      <div className="flex flex-col flex-start gap-10 text-justify leading-8">
+        {blogContent.header && (
+          <div>
+            <h1>{blogContent.header.title}</h1>
+            <div className="flex flex-start">
+              <p>
+                {blogContent.header.author} - {blogContent.header.date}
+              </p>
+            </div>
+            <br />
+            <Typography>{blogContent.header.description}</Typography>
+          </div>
+        )}
+
+        {blogContent.body &&
+          blogContent.body.map((paragraph) => (
+            <div key={paragraph.subTitle}>
+              <h3>{paragraph.subTitle}</h3>
+              <br />
+              <Typography>{paragraph.description}</Typography>
+            </div>
+          ))}
+
+        {blogContent.summary && <Typography>{blogContent.summary}</Typography>}
+      </div>
+
+      <br />
+
+      <h3>References</h3>
+      <div>
+        {blogContent.references &&
+          blogContent.references.map((reference) => (
+            <ul key={reference.name} className="list-disc pl-8">
+              <Link href={reference.link} target="_blank" alt={reference.link}>
+                <li>{reference.name}</li>
+              </Link>
+            </ul>
+          ))}
+      </div>
+    </Fade>
+  );
+}
