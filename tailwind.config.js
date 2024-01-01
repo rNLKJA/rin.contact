c975b6d0bd24ad75dee7eaa89345d84c57b0a5d5
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-01 18:40:36 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       maxWidth: {
-        custom: "1100px", // Custom maxWidth class
+        custom: "1200px",
       },
     },
   },
