200e58a2be3fe7de213bc6a9e705a471df21527f
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-01 18:24:31 +1100

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,14 +3,11 @@ import Image from "next/image";
 
 const Custom404 = () => {
   return (
-    <div
-      className="flex md:flex-row h-full justify-center items-center gap-10"
-      style={{ padding: "50px", textAlign: "center" }}
-    >
+    <div className="flex md:flex-row w-full  justify-center items-center gap-10">
       <div>
-        <Image src="/404.png" alt="404.png" width={300} height={300} />
+        <Image src="/404.png" alt="404.png" width={250} height={250} />
       </div>
-      <div>
+      <div className="flex flex-col text-center gap-2">
         <h1>404</h1>
         <p>Oops! The page you're looking for was not found.</p>
       </div>
