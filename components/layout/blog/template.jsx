14cf2164d5e28e647fdea10f68cfdcb848935681
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-05 01:06:01 +1100

    Update template.jsx

--- components/layout/blog/template.jsx
+++ components/layout/blog/template.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, use } from "react";
+import React, { useState, useEffect } from "react";
 import { Fade } from "react-awesome-reveal";
 
 export default function BlogContent() {
