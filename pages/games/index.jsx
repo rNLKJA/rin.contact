79806d7d4bb6210859e96155f04030bac9184284
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-05 12:38:27 +1100

    Update index.jsx

--- pages/games/index.jsx
+++ pages/games/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { DinoGame } from "@/components/game/DinoGame";
+import Link from "next/link";
 
 const Game = () => {
   return (
     <div>
-      <DinoGame />
+      <h2>Web Game List</h2>
+      <ul className="list-disc pl-8">
+        <li className="link-hover">
+          <Link href="/games/dino">Dinosaur (incomplete)</Link>
+        </li>
+        <li className="link-hover">
+          <Link href="/games/guess-the-number">Guess the number</Link>
+        </li>
+      </ul>
     </div>
   );
 };
