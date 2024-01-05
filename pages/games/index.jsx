c4214aded63e1547326bfa42fb8a00f17cfdb682
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-05 12:29:53 +1100

    Added dino file

---
+++ pages/games/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DinoGame } from "@/components/game/DinoGame";
+
+const Game = () => {
+  return (
+    <div>
+      <DinoGame />
+    </div>
+  );
+};
+
+export default Game;
