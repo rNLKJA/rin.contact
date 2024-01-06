8acfa91a77daead4a72bab2325ee79a7914982ac
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-06 16:21:01 +1100

    Updated Head

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -35,6 +35,46 @@ function MyApp({ Component, pageProps }) {
     >
       <Head>
         <title>Pawsibly Rin</title>
+        <meta
+          name="description"
+          content="Data Scientist Rin Huang: Explore my tech insights and data-driven solutions to real-world problems using modern tools"
+        />
+        <meta name="author-full-name" content="Sunchuangyu Huang" />
+        <meta name="author-prefer-name" content="Rin Huang" />
+        <meta name="author-chinese-name" content="黄孙创宇" />
+        <meta name="author-email" content="huangsunchuangyu@gmail.com" />
+        <meta name="author-github" content="https://github/rNLKJA" />
+        <meta
+          name="author-linkedin"
+          content="https://www.linkedin.com/in/sunchuangyuhuang/"
+        />
+        <meta name="author-website" content="https://rin.contact" />
+        <meta
+          name="instagram"
+          content="https://www.instagram.com/chuangyu_hscy"
+        />
+        <meta
+          name="xiaohongshu"
+          content="https://www.xiaohongshu.com/user/profile/5ddb3cf2000000000100bcab"
+        />{" "}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://rin.contact/" />
+        <meta property="og:title" content="rNLKJA's Portfolio" />
+        <meta
+          property="og:description"
+          content="Explore Rin Huang's data science projects and read his latest thoughts on tech, data science and life."
+        />
+        <meta
+          property="og:image"
+          content="https://rin.contact/meta-image.png"
+        />{" "}
+        <meta property="highschool" content="安顺第二高级中学" />
+        <meta property="高中" content="安顺第二高级中学" />
+        <meta property="college" content="trinity college" />
+        <meta property="姓名" content="Sunchuangyu Huang" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#f5f5f5" />
       </Head>
       <Header />
       <AnimatePresence mode="wait">
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,43 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            name="description"
-            content="Data Scientist Rin Huang: Explore my tech insights and data-driven solutions to real-world problems using modern tools"
-          />
-          <meta name="author-full-name" content="Sunchuangyu Huang" />
-          <meta name="author-prefer-name" content="Rin Huang" />
-          <meta name="author-chinese-name" content="黄孙创宇" />
-          <meta name="author-email" content="huangsunchuangyu@gmail.com" />
-          <meta name="author-github" content="https://github/rNLKJA" />
-          <meta
-            name="author-linkedin"
-            content="https://www.linkedin.com/in/sunchuangyuhuang/"
-          />
-          <meta name="author-website" content="https://rin.contact" />
-          <meta
-            name="instagram"
-            content="https://www.instagram.com/chuangyu_hscy"
-          />
-          <meta
-            name="xiaohongshu"
-            content="https://www.xiaohongshu.com/user/profile/5ddb3cf2000000000100bcab"
-          />
-
           <link rel="icon" href="/favicon.ico" />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://rin.contact/" />
-          <meta property="og:title" content="rNLKJA's Portfolio" />
-          <meta
-            property="og:description"
-            content="Explore Rin Huang's data science projects and read his latest thoughts on tech, data science and life."
-          />
-          <meta
-            property="og:image"
-            content="https://rin.contact/meta-image.png"
-          />
-
           <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
@@ -67,20 +31,9 @@ class MyDocument extends Document {
               }),
             }}
           />
-
           <link rel="canonical" href="https://rin.contact" />
           <link rel="alternate" hrefLang="x" href="https://x.rin.contact/" />
-
-          <meta property="highschool" content="安顺第二高级中学" />
-          <meta property="高中" content="安顺第二高级中学" />
-          <meta property="college" content="trinity college" />
-          <meta property="姓名" content="Sunchuangyu Huang" />
-
-          <meta charSet="utf-8" />
-
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="canonical" href="https://rin.contact" />
-          <meta name="theme-color" content="#f5f5f5" />
         </Head>
         <body>
           <Main />
