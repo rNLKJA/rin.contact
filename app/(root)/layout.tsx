64d4e3e05bb8b91f4b097c9921a4d5d58707bb84
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-01-24 16:55:22 +1100

    updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Narbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <React.Fragment>
